9d70c3633e30037f16364b2408a5faccb378d77a
bhushan <bhushan011294@gmail.com>
2025-11-11 15:06:07 +0530

    LastCommit

--- tests/demo1.spec.js
+++ tests/demo1.spec.js
@@ -7,15 +7,15 @@ test ('Locators' , async ({page}) =>
 
   await page.getByLabel('Username').fill('Chandler');
   await page.getByLabel('Password').fill('Chandler@123');
- //await page.screenshot({path : 'screenshot/todo-final.png', fullPage:true});
-  //await page.getByRole ('link' , {name : 'Close'}).click();
- // await expect(page.getByText ('Samsung')).toBeVisible();
+ await page.screenshot({path : 'screenshot/todo-final.png', fullPage:true});
+  await page.getByRole ('link' , {name : 'Close'}).click();
+ await expect(page.getByText ('Samsung')).toBeVisible();
 
-  //await page.getByRole ('button', {name : 'Sign up'}).click();
+  await page.getByRole ('button', {name : 'Sign up'}).click();        
 
-// await page.getByRole('link' , {name : 'About us'}).click();
- // await page.getByTitle ('play video').click();
+await page.getByRole('link' , {name : 'About us'}).click();
+ await page.getByTitle ('play video').click();
 
 
- // await page.getByLabel
+ await page.getByLabel
 });
