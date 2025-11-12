2b4bd6b48c53661414d4db225cd6d4e9200d4522
bhushan <bhushan011294@gmail.com>
2025-11-12 10:23:59 +0530

    bm

--- tests/demo1.spec.js
+++ tests/demo1.spec.js
@@ -2,17 +2,23 @@ import {test , expect} from '@playwright/test'
 
 test ('Locators' , async ({page}) =>
     {
- await page.goto('https://practice.expandtesting.com/inputs');
+
+     await page.goto('https://practice.expandtesting.com/upload');
+     await page.locator("#fileInput").setInputFiles("./file/play.jpg");
+     //await page.waitForTimeout(3000);
+     await page.locator("#fileSubmit").click();
+     await expect(page.locator('h1').getByText("File Uploaded!")).toBeVisible();
+//  await page.goto('https://practice.expandtesting.com/inputs');
 
 
- await page.locator('#input-number').fill('9874563210');
- await page.locator('#input-text').fill('Hello India !');
- await page.locator('#input-password').fill('User123');
+//  await page.locator('#input-number').fill('9874563210');
+//  await page.locator('#input-text').fill('Hello India !');
+//  await page.locator('#input-password').fill('User123');
 
 
- await page.locator('#btn-display-inputs').click();
- await page.waitForTimeout(3000);
- await page.locator('#btn-clear-inputs').click();
+//  await page.locator('#btn-display-inputs').click();
+//  await page.waitForTimeout(3000);
+//  await page.locator('#btn-clear-inputs').click();
   // await page.getByRole('link', {name :'Sign up'}).click();
 
   // await page.getByLabel('Username').fill('Chandler');
